91ec0af3f8f3cc1fe04440ae1fb99310d47d4b42
soderlind <per@soderlind.no>
2019-12-11 22:36:38 +0100

    1.0.0

---
+++ es6-wp-ajax-demo.js
@@ -0,0 +1,40 @@
+/**
+ * ES6 WP Ajax Demo
+ *
+ * Uses ajax_es6_ajax_action() in es6-wp-ajax-demo.php
+ *
+ * @author Per Soderlind <per@soderlind.no>
+ *
+ */
+document.addEventListener('DOMContentLoaded', e => {
+	let button = document.getElementById('es6-demo-input');
+	let output = document.getElementById('es6-demo-output');
+
+	button.onclick = event => {
+		event.preventDefault();
+
+		const self = event.currentTarget;
+		const data = new FormData();
+		data.append('action', 'es6_ajax_action');
+		data.append('nonce', pluginES6WPAjax.nonce);
+		data.append('sum', self.dataset.sum);
+
+		const url = pluginES6WPAjax.ajaxurl + '?now=' + escape(new Date().getTime().toString());
+		fetch(url, {
+			method: 'POST',
+			credentials: 'same-origin',
+			body: data
+		}).then(res => res.json())
+			.then(res => {
+				if (res.response == 'success') {
+					self.dataset.sum = res.data;
+					output.innerHTML = res.data;
+				} else {
+					console.error(res);
+				}
+			})
+			.catch(err => {
+				console.error(err);
+			})
+	};
+});
---
+++ es6-wp-ajax-demo.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * ES6 WP Ajax Demo
+ *
+ * @package     ES6 WP Ajax Demo
+ * @author      Per Soderlind
+ * @copyright   2018 Per Soderlind
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: ES6 WP Ajax Demo
+ * Plugin URI: https://github.com/soderlind/es6-wp-ajax-demo
+ * GitHub Plugin URI: https://github.com/soderlind/es6-wp-ajax-demo
+ * Description: Use native JavaScript (ES6) when doing Ajax calls.
+ * Version:     1.0.0
+ * Author:      Per Soderlind
+ * Author URI:  https://soderlind.no
+ * Text Domain: es6-wp-ajax-demo
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+declare( strict_types = 1 );
+namespace Soderlind\Demo\Ajax;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+const ES6_WP_AJAX_DEMO_VERSION = '1.0.0';
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\wp_scripts' );
+add_action( 'wp_ajax_es6_ajax_action', __NAMESPACE__ . '\es6_ajax_action' );
+
+function es6_ajax_action() {
+	header( 'Content-type: application/json' );
+	if ( check_ajax_referer( 'es6_wp_ajax_nonce', 'nonce', false ) ) {
+		$sum = ( isset( $_POST['sum'] ) ) ? filter_var( wp_unslash( $_POST['sum'] ), FILTER_VALIDATE_INT, [ 'default' => 0 ] ) : 0;
+		if ( isset( $sum ) ) {
+			$response['response'] = 'success';
+			$sum                  = $sum + 1;
+			$response['data']     = $sum;
+			update_option( 'es6demo_sum', $sum );
+		} else {
+			$response['response'] = 'failed';
+			$response['data']     = 'something went wrong ...';
+		}
+	} else {
+		$response['response'] = 'failed';
+		$response['message']  = 'invalid nonse';
+	}
+	echo wp_json_encode( $response );
+	wp_die();
+}
+
+function wp_scripts() {
+
+	$ajaxurl = get_ajax_url();
+	$url     = plugins_url( '', __FILE__ );
+
+	// Load fetch polyfill, url via https://polyfill.io/v3/url-builder/.
+	wp_enqueue_script( 'polyfill-fetch', 'https://polyfill.io/v3/polyfill.min.js?features=fetch', [], ES6_WP_AJAX_DEMO_VERSION, true );
+	wp_enqueue_script( 'es6-wp-ajax', $url . '/es6-wp-ajax-demo.js', [ 'polyfill-fetch' ], ES6_WP_AJAX_DEMO_VERSION, true );
+	$data = wp_json_encode(
+		[
+			'nonce'   => wp_create_nonce( 'es6_wp_ajax_nonce' ),
+			'ajaxurl' => $ajaxurl,
+		]
+	);
+	wp_add_inline_script( 'es6-wp-ajax', "const pluginES6WPAjax = ${data};" );
+}
+
+function get_ajaxurl() : string {
+	// multisite fix, use home_url() if domain mapped to avoid cross-domain issues.
+	$http_scheme = ( is_ssl() ) ? 'https' : 'http';
+	if ( home_url() !== site_url() ) {
+		$ajaxurl = home_url( '/wp-admin/admin-ajax.php', $http_scheme );
+	} else {
+		$ajaxurl = site_url( '/wp-admin/admin-ajax.php', $http_scheme );
+	}
+	return $ajaxurl;
+}
+
+/**
+ * Demo Form
+ */
+add_shortcode( 'es6demo', __NAMESPACE__ . '\es6demo_form' );
+
+function es6demo_form( $args ) {
+	$o = '';
+
+	$sum = get_option( 'es6demo_sum', 0 );
+	$o  .= '<div id="es6-demo">';
+	// $o .= 	'<div id="es6-demo-form">';
+	$o .= '<div id="es6-demo-output">' . $sum . '</div>';
+	$o .= '<form><input id="es6-demo-input" type="button" value="+" data-sum="' . $sum . '"></form>';
+	// $o .= 	'</div>';
+	$o .= '</div>';
+
+	return $o;
+}
+
+add_action(
+	'wp_enqueue_scripts',
+	function() {
+		$url = plugins_url( '', __FILE__ );
+		wp_enqueue_style( 'es6-wp-form', $url . '/es6-wp-ajax-demo.css', [], ES6_WP_AJAX_DEMO_VERSION );
+	}
+);
+
