90536b30c6e9d8237b87b8bf44ead8713bd5bfa4
soderlind <per@soderlind.no>
2019-12-12 21:55:00 +0100

    Use async/await

--- es6-wp-ajax-demo.js
+++ es6-wp-ajax-demo.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', e => {              // Wait util t
     let button = document.getElementById('es6-demo-input');       // The form button
     let output = document.getElementById('es6-demo-output');      // The output area
 
-    button.onclick = event => {                                   // Fire event when the button is clicked.
+     button.onclick = event = async (event) => {                  // Fire the event when the button is clicked.
         event.preventDefault();
 
         const self = event.currentTarget;                         // "this" button
@@ -18,23 +18,26 @@ document.addEventListener('DOMContentLoaded', e => {              // Wait util t
         data.append('action', 'es6_ajax_action');                 // wp ajax action, more info at https://developer.wordpress.org/plugins/javascript/enqueuing/#ajax-action
         data.append('nonce', pluginES6WPAjax.nonce);              // set the nonce, added at https://github.com/soderlind/es6-wp-ajax-demo/blob/master/es6-wp-ajax-demo.php#L75
         data.append('sum', self.dataset.sum);                     // get the value from the data-sum attribute in the form.
+
         const url = pluginES6WPAjax.ajaxurl;                      // set the ajax url, added at https://github.com/soderlind/es6-wp-ajax-demo/blob/master/es6-wp-ajax-demo.php#L76
-        fetch(url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            body: data
-        }).then(res => res.json())                                // read the json response from https://github.com/soderlind/es6-wp-ajax-demo/blob/master/es6-wp-ajax-demo.php#L57
-            .then(res => {
-                if (res.response == 'success') {
-                    self.dataset.sum = res.data;                  // Update the data-sum attribute with the incremented value.
-                    output.innerHTML = res.data;                  // Display the updated value.
-                    console.log(res);
-                } else {
-                    console.error(res);
-                }
-            })
-            .catch(err => {
-                console.error(err);
-            })
+        try {
+            const response = await fetch(url, {                   // POST the data to WordPress
+                method: 'POST',
+                credentials: 'same-origin',
+                body: data
+            });
+
+            const res = await response.json();                    // read the json response from https://github.com/soderlind/es6-wp-ajax-demo/blob/master/es6-wp-ajax-demo.php#L57
+            if (res.response == 'success') {
+                self.dataset.sum = res.data;                      // Update the data-sum attribute with the incremented value.
+                output.innerHTML = res.data;                      // Display the updated value.
+                console.log(res);
+            } else {
+                console.error(res);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
     };
 });
